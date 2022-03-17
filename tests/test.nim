698326a1d7b5d6eb0d5c9419f6f0a0ba2ea87efd
bichanna <nobu.bichanna@gmail.com>
2022-03-16 20:01:29 -0500

    Add some comments

--- tests/test.nim
+++ tests/test.nim
@@ -1,5 +1,13 @@
+#[
+  tests/test.nim
+
+  Written by Nobuharu Shimazu
+  Date: 2022/3/16
+]#
+
 import unittest
 
 import balgos/mult
 test "can third-grade mult":
   check third_grade_mult(12345, 6789) == 83810205
+  check third_grade_mult(1234, 5678) == 7006652
